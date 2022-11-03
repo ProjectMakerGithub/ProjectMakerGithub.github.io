f41d74bda2bca2332b422a8ac2803670a73683b4
Project Maker <112566873+ProjectMakerGithub@users.noreply.github.com>
2022-11-03 12:24:18 +0100

    commit -> cloudflare -> 0.0.0

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\Users\\Léo LB\\Documents\\ProjectMaker\\project_maker_documentation\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\Léo LB\\Documents\\ProjectMaker\\project_maker_documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\Léo LB\\Documents\\ProjectMaker\\project_maker_documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\Léo LB\\Documents\\ProjectMaker\\project_maker_documentation\\src\\css\\custom.css'),
+];
